40bb6180cd697794851e1a26d18c23a77b263ad7
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-06 17:37:53 +0530

    done

--- src/app/page-container/page-container.component.ts
+++ src/app/page-container/page-container.component.ts
@@ -1,8 +1,25 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  Input,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-page-container',
   templateUrl: './page-container.component.html',
   styleUrls: ['./page-container.component.css']
 })
-export class PageContainerComponent {}
+export class PageContainerComponent implements OnInit {
+  @ViewChild('para', { static: true }) pTag: ElementRef;
+
+  ngOnInit(): void {
+    this.pTag.nativeElement.textContent = 'My paragraph';
+    this.pTag.nativeElement.style.color = 'red';
+    this.pTag.nativeElement.className = 'font-weight-bold';
+    this.pTag.nativeElement.style.border = '1px solid blue';
+  }
+}
