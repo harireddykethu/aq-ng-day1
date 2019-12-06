54dd98d27c4100768d966a4995be5ab8939cb592
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-06 17:20:33 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  counter = 0;
-
-  incrementHandler() {
-    if (this.counter < 25) {
-      this.counter++;
-    } else {
-      console.log('Limit breached');
-    }
-  }
-
-  decrementHandler() {
-    if (this.counter > 0) {
-      this.counter--;
-    } else {
-      console.log('Limit breached');
-    }
-  }
-}
+export class AppComponent {}
--- src/app/page-container/page-container.component.ts
+++ src/app/page-container/page-container.component.ts
@@ -5,16 +5,4 @@ import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
   templateUrl: './page-container.component.html',
   styleUrls: ['./page-container.component.css']
 })
-export class PageContainerComponent {
-  @Input() childCounter: number;
-  @Output() increment = new EventEmitter();
-  @Output() decrement = new EventEmitter();
-
-  incrementClickHandler() {
-    this.increment.emit();
-  }
-
-  decrementClickHandler() {
-    this.decrement.emit();
-  }
-}
+export class PageContainerComponent {}
