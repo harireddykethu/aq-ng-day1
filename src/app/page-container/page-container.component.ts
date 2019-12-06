876a2c1744ffc40ac993b37fd63975b082bf333a
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-06 16:25:48 +0530

    done

--- src/app/page-container/page-container.component.ts
+++ src/app/page-container/page-container.component.ts
@@ -1,29 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-page-container',
   templateUrl: './page-container.component.html',
   styleUrls: ['./page-container.component.css']
 })
-export class PageContainerComponent implements OnInit {
-  title = 'Hello there';
-  pi = 3.141569;
-  amount = 100.235;
-  rate = 0.78;
-  today = new Date();
-
-  foo() {
-    //...
-    //console.log('called');
-    return {
-      name: 'Vijay',
-      city: 'Bengaluru'
-    };
-  }
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.title = 'Changed!';
-    }, 3000);
-  }
+export class PageContainerComponent {
+  @Input() title: string;
 }
